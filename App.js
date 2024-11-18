b15a948e5a3c701c4e4ee430478e70f4f9f1ff25
gwanho <ykh4343@naver.com>
2024-11-19 00:02:56 +0900

    LastRegisterScreen 작성 완료 및 app.js에 추가

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ import FindPasswordScreen from './screens/FindPasswordScreen';
 import SetPassWordScreen from './screens/SetPassWordScreen';
 import RegisterScreen from './screens/RegisterScreen';
 import SelectListScreen from './screens/SelectListScreen';
+import LastRegisterScreen from './screens/LastRegisterScreen';
 
 
 const Stack = createStackNavigator();
@@ -19,6 +20,7 @@ export default function App() {
         <Stack.Screen name="SetPassWordScreen" component={SetPassWordScreen} />
         <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
         <Stack.Screen name ="SelectListScreen" component={SelectListScreen} />
+        <Stack.Screen name ="LastRegisterScreen" component={LastRegisterScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );  
